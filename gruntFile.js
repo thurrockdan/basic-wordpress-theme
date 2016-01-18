55fcc0f32d314c47c8ab04e37b0450826d3db5ea
thurrockdan <daniel@ecomstudio.co.uk>
2016-01-18 14:52:45 +0000

    updates to environment

--- gruntFile.js
+++ gruntFile.js
@@ -15,22 +15,49 @@ module.exports = function(grunt) {
                                         style: 'compressed',
             				compass: false
         			},
-        		files: {
-            			'<%= project.assets %>/css/style.css':'<%= project.css %>'
-        		}
+                    files: {
+                            '<%= project.assets %>/dist/css/style.css':'<%= project.css %>'
+                    }
     			}
 		},
+        uglify: {
+            dev: {
+                files: {
+                    '<%= project.assets %>/dist/js/script.min.js': '<%= project.assets %>/js/script.js'
+                }
+            }
+        },
 		watch: {
     			sass: {
         			files: '<%= project.assets %>/sass/{,*/}*.{scss,sass}',
         			tasks: ['sass:dev']
-    			}
-		}
+    			},
+                uglify: {
+                    files: '<%= project.assets %>/js/{,*/}*.js',
+                    tasks: ['uglify:dev']
+                }
+		},
+		bower: {
+            dev: {
+                base: 'bower_components',
+                dest: '<%= project.assets %>/bower_components',
+                options: {
+                    checkExistence: true,
+                    debugging: true,
+                    paths: {
+                        bowerDirectory: 'bower_components',
+                        bowerrc: '.bowerrc',
+                        bowerJson: 'bower.json'
+                    }
+                }
+            }
+        }
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('main-bower-files');
 	grunt.registerTask('default', [
 
 	]);
