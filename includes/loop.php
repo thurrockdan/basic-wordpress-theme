40d313ed2b863255e50ab43842cc5eccd2df21ce
thurrockdan <daniel@ecomstudio.co.uk>
2016-01-18 15:33:26 +0000

    updates to environment

--- includes/loop.php
+++ includes/loop.php
@@ -1,24 +1,10 @@
 <!-- Start the Loop. -->
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-
-
  	<h1><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 
 
- 	<small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
-
-
-
-
  		<?php the_content(); ?>
-
-
-
- 	<p class="postmetadata"><?php _e( 'Posted in' ); ?> <?php the_category( ', ' ); ?></p>
-
-
-
 
  <?php endwhile; else : ?>
 
